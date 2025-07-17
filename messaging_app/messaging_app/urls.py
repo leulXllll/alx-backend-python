6828ba11dba5329964c988071f1adf8304125229
Leul <leulzewduchanie@gmail.com>
2025-07-17 12:22:52 +0300

    added api-auth

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -14,11 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Make sure include is imported
+
+from rest_framework_nested.routers import NestedDefaultRouter
+from chats.views import ConversationViewSet, MessageViewSet
+
+# Base router
+router = NestedDefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+
+# Nested messages router
+conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    
+    path('api/', include(conversations_router.urls)),
+    path('api-auth/', include('rest_framework.urls')),  # Enables the browsable API login/logout
 ]
