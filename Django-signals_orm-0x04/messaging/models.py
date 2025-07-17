28302c6d94f5c22b0c3c231aeb8da0056b96acd2
Leul <leulzewduchanie@gmail.com>
2025-07-17 12:59:29 +0300

    modified signal.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,15 +6,23 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
+    edited_at = models.DateTimeField(null=True, blank=True)
+    edited_by = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='edited_messages'
+    )
 
     def __str__(self):
-        return f'From {self.sender} to {self.receiver}: {self.content[:30]}'
+        return f'{self.sender} to {self.receiver}: {self.content[:30]}'
 
-class Notification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
-    message = models.ForeignKey(Message, on_delete=models.CASCADE)
-    is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Notification for {self.user.username} - Message ID: {self.message.id}'
+        return f'History of Message ID {self.message.id}'
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,18 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, MessageHistory
 
-@receiver(post_save, sender=Message)
-def create_notification_on_new_message(sender, instance, created, **kwargs):
-    if created:
-        Notification.objects.create(user=instance.receiver, message=instance)
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:  # Only run on updates
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:
+                # Log old content
+                MessageHistory.objects.create(
+                    message=instance,
+                    old_content=old_message.content
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
